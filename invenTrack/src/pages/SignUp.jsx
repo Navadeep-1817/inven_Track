35b9681b13af35220926e2f445e6463236075308
Navadeep <navadeep1817@gmail.com>
2025-11-05 20:25:30 +0530

    vercel-start-1

--- invenTrack/src/pages/SignUp.jsx
+++ invenTrack/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom"; // ✅ import navigation
-import "../styles/SignUp.css";
+import "../styles/Signup.css";
 
 function Signup() {
   const [form, setForm] = useState({
